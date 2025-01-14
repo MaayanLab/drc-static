3b4ba3fbe94db75b5126db500eccf690e9a46877
Daniel Clarke <u8sand@gmail.com>
2025-01-14 11:20:17 -0500

    Very basic example of centers/dccs pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
+	integrations: [react(), mdx()],
 });
